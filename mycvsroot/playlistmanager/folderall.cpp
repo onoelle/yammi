06755ca84a575358d9b76dd13217bbdc3eb034eb
Oliver Nölle <oli.noelle@web.de>
2001-12-12 17:07:53 +0000

    test3

--- mycvsroot/playlistmanager/folderall.cpp
+++ mycvsroot/playlistmanager/folderall.cpp
@@ -14,7 +14,7 @@
  *   (at your option) any later version.                                   *
  *                                                                         *
  ***************************************************************************/
-
+// olitest
 #include "folderall.h"
 
 FolderAll::FolderAll( QListView* parent, QString title)
